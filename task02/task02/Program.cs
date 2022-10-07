e8761025e2a1eef528c912bc052ed67253ff1a69
DariaKosykh <Daria.Kosykh@urfu.me>
2022-10-07 11:23:51 +0500

    Задание 2 - треугольник

---
+++ task02/task02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите значение стороны a");
+            a = int.Parse(Console.ReadLine());
+            int b;
+            Console.WriteLine("Введите значение стороны b");
+            b = int.Parse(Console.ReadLine());
+            int gradus;
+            Console.WriteLine("Введите значение угла между ними (в градусах)");
+            gradus = int.Parse(Console.ReadLine());
+            double radian = gradus * Math.PI / 180;
+            double S = (a * b * Math.Sin(radian)) / 2;
+            Console.WriteLine("Площадь равна "  + S);
+            double P =  Math.Sqrt(a * a + b * b - 2 * b * a * Math.Cos(radian)) + a + b;
+            Console.WriteLine("Периметр равен " + P);
+            Console.ReadKey(); 
+        }
+    }
+}
