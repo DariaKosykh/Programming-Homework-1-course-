da1e30c42f50d63ac38b4e0695b3107822e1b300
DariaKosykh <Daria.Kosykh@urfu.me>
2022-10-11 13:57:10 +0500

    Задание 3 - Целочисленная арифметика

---
+++ task03/task03/task03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите четырехзначное число");
+            a = int.Parse(Console.ReadLine());
+            int a1 = a / 10;
+            Console.WriteLine("Число десятков " + a1);
+            int a2 = a / 100;
+            Console.WriteLine("Число сотен " + a2);
+            int s = 0;
+            while (a > 0)
+            {
+                s = s + a % 10;
+                a = a / 10;
+            }
+            Console.WriteLine("Cумма цифр четырёхзначного числа " + s);
+            Console.ReadKey();
+
+        }
+    }
+}
