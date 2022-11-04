5c5d83ab4a172fd3606cbfcd97aa94135cc4f697
DariaKosykh <Daria.Kosykh@urfu.me>
2022-11-04 15:19:45 +0500

    Задание 5 - Выделение метода

---
+++ task05/task05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1;
+             P(3, 11,out x1);
+            double x2;
+             P(5, 13, out x2);
+            double x3;
+             P(7, 17, out x3);
+            double x = x1 + x2 + x3;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+        static void P(int a,int b, out double t)
+        {
+            t = (1 + Math.Pow(a, 2)) / (1 + Math.Pow(b, 2));
+        }
+}   }
+       
+       
+ 
+
+
