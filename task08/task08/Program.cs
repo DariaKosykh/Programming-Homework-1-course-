7b9b974661555ce8d1f6c9c6a56002bd0917ef0b
DariaKosykh <Daria.Kosykh@urfu.me>
2022-11-28 12:28:12 +0500

    Задание 8 - Условные операторы

---
+++ task08/task08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = int.Parse(Console.ReadLine());
+            FunctionEvaluation(ref x);
+            Console.WriteLine("F(x) = " + FunctionEvaluation(ref x));
+            Console.ReadKey();
+        }
+    
+        static double FunctionEvaluation(ref double x)
+        {
+            if ((Math.Abs(x)) < (Math.PI / 4))
+            { 
+                return Math.Atan(x) * 180 / Math.PI;
+            }
+            else if (x >= (Math.PI / 4))
+            {
+               return 1;
+            }
+            else
+            {
+                return -1;
+            }
+
+        } 
+
+}
+    }
