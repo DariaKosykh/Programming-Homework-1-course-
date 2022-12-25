e94dc694299831ad8f44c2b04ee54769967a21d1
DariaKosykh <Daria.Kosykh@urfu.me>
2022-12-25 10:51:03 +0500

    Задание 9 - Операторы цикла

---
+++ task09/task09.3/task09.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task09._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 3.Введите сколько лыжник пробежал в первый день (n)");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ваедите на сколько процентов спротсмен увеличивал бег каждый день (m)");
+            double m = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение, которое привысит спортсмен (k)");
+            double k = double.Parse(Console.ReadLine());
+            int day = 0;
+            double Sum = 0;
+            do
+            {
+                day++;
+                Sum = Sum + n * (1 + m / 100);
+                n = n * (1 + m / 100);
+            }
+            while (Sum < k);
+            Console.WriteLine("Сколько дней бегал спортсмен " + day);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task09/task09.4/task09.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task09._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10000; i > 0; i--)
+            {
+                int m = 1, sum = default;
+                while (m != i)
+                {
+                    if (i % m == 0)  sum += m;
+                    m++;
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
