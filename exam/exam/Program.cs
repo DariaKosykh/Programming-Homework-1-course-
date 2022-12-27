21408c23244606384097bd08c7b537acbd5ced86
DariaKosykh <Daria.Kosykh@urfu.me>
2022-12-27 15:09:04 +0500

    Зачёт - билет 2

---
+++ exam/exam/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите чётное натуральное число, которое болшьше или равно 4");
+            int n = int.Parse(Console.ReadLine());
+            if (NumberСheck(n))
+            {
+                for (int a = 2; a < n; a++)
+                {
+                    if (CheckSimpleNumber(a))
+                    {
+                        for (int b = 2; b < n; b++)
+                        {
+                            if (CheckSimpleNumber(b))
+                            {
+                                if (a + b == n)
+                                {
+                                    Console.WriteLine($"{a} + {b} = {n}");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+            
+        static bool CheckSimpleNumber(int n)
+        {
+            if (n == 1) return false;
+            for ( int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+        }
+        static bool NumberСheck(int n)
+        { 
+            if ((n % 2 == 0) & (n >= 4))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Число задано не верно. Попробуйте снова.");
+                return false;
+            }
+        }
+    } 
+}
