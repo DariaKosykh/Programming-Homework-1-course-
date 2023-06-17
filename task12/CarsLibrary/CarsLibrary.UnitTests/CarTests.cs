d27aa0076dc081a8e79c4230fe0935d238a16cab
DariaKosykh <Daria.Kosykh@urfu.me>
2023-06-17 23:22:11 +0500

    Задание 13 - Наследование и полиморфизм

--- task12/CarsLibrary/CarsLibrary.UnitTests/CarTests.cs
+++ task12/CarsLibrary/CarsLibrary.UnitTests/CarTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Diagnostics.Contracts;
 using static CarsLibrary.Cars;
 
 namespace CarsLibrary.UnitTests
@@ -40,5 +41,99 @@ namespace CarsLibrary.UnitTests
                 Assert.That(result, Is.EqualTo(expectedString));
             }
         }
-    }
+        public class NewCarTests
+        {
+            [Test]
+            public void NewCar_ValidArguments_CreateNewCar()
+            {
+                var newCar = new NewCar("Hundai", 2000, "12345678901234567", BodyType.Sedan, "Factory 1",
+                    TimeSpan.FromDays(365));
+
+                Assert.That(newCar.Brand, Is.EqualTo("Hundai"));
+                Assert.That(newCar.YearOfRelease, Is.EqualTo(2000));
+                Assert.That(newCar.VIN, Is.EqualTo("12345678901234567"));
+                Assert.That(newCar.BodyType, Is.EqualTo(BodyType.Sedan));
+                Assert.That(newCar.Factory, Is.EqualTo("Factory 1"));
+                Assert.That(newCar.GuaranteeTime, Is.EqualTo(TimeSpan.FromDays(365)));
+            }
+
+            [Test]
+            public void GetInfo_NewCar_ReturnsCorrectString()
+            {
+                const string expectedString = $"Brand: Hundai. Year of release: 2000. VIN: 12345678901234567. Body type: Sedan \n"
+                                              + $"Price: 0. Date of sale: 01.01.0001. Customer full name: \n"
+                                                + "Factory: Factory 1. Guarantee time: 365 days";
+
+                var newCar = new NewCar("Hundai", 2000, "12345678901234567", BodyType.Sedan, "Factory 1",
+                    TimeSpan.FromDays(365));
+
+                var result = newCar.GetInfo();
+
+                Assert.That(result, Is.EqualTo(expectedString));
+            }
+        }
+        public class UsedCarTests
+        {
+            [Test]
+            public void UsedCar_ValidArguments_CreateCar()
+            {
+                var usedCar = new UsedCar("Hundai", 2000, "12345678901234567", BodyType.Sedan, 1000, CarCondition.Excellent);
+
+                Assert.That(usedCar.Brand, Is.EqualTo("Hundai"));
+                Assert.That(usedCar.YearOfRelease, Is.EqualTo(2000));
+                Assert.That(usedCar.VIN, Is.EqualTo("12345678901234567"));
+                Assert.That(usedCar.BodyType, Is.EqualTo(BodyType.Sedan));
+                Assert.That(usedCar.Mileage, Is.EqualTo(1000));
+                Assert.That(usedCar.Condition, Is.EqualTo(CarCondition.Excellent));
+            }
+
+            [Test]
+            public void GetInfo_UsedCar_ReturnsCorrectString()
+            {
+                const string expectedString = $"Brand: Hundai. Year of release: 2000. VIN: 12345678901234567. Body type: Sedan \n"
+                                              + $"Price: 0. Date of sale: 01.01.0001. Customer full name: \n"
+                                              + $"Mileage: 1000. Condition: Excellent";  
+                var usedCar = new UsedCar("Hundai", 2000, "12345678901234567", BodyType.Sedan, 1000, CarCondition.Excellent);
+
+                var result = usedCar.GetInfo();
+
+                Assert.That(result, Is.EqualTo(expectedString));
+            }
+        }
+        public class CommissionCarTests
+        {
+            [Test]
+            public void CommissionCar_ValidArguments_CreateCar()
+            {
+                var commissionCar = new CommissionCar("Hundai", 2000, "12345678901234567", BodyType.Sedan, 1000,
+                    CarCondition.Excellent, "Ivanov Ivan Ivanovich", "Lenin Street 6", "123");
+
+                Assert.That(commissionCar.Brand, Is.EqualTo("Hundai"));
+                Assert.That(commissionCar.YearOfRelease, Is.EqualTo(2000));
+                Assert.That(commissionCar.VIN, Is.EqualTo("12345678901234567"));
+                Assert.That(commissionCar.BodyType, Is.EqualTo(BodyType.Sedan));
+                Assert.That(commissionCar.Mileage, Is.EqualTo(1000));
+                Assert.That(commissionCar.Condition, Is.EqualTo(CarCondition.Excellent));
+                Assert.That(commissionCar.OwnerFullName, Is.EqualTo("Ivanov Ivan Ivanovich"));
+                Assert.That(commissionCar.OwnerAddress, Is.EqualTo("Lenin Street 6"));
+                Assert.That(commissionCar.ContractNumber, Is.EqualTo("123"));
+            }
+
+            [Test]
+            public void GetInfo_CommissionCar_ReturnsCorrectString()
+            {
+                const string expectedString = $"Brand: Hundai. Year of release: 2000. VIN: 12345678901234567. Body type: Sedan \n"
+                                              + $"Price: 0. Date of sale: 01.01.0001. Customer full name: \n"
+                                              + $"Mileage: 1000. Condition: Excellent \n"
+                                              + $"Owner full name:Ivanov Ivan Ivanovich . Owner address: Lenin Street 6. Contract number: 123.";;
+                var commissionCar = new CommissionCar("Hundai", 2000, "12345678901234567", BodyType.Sedan, 1000,
+                    CarCondition.Excellent, "Ivanov Ivan Ivanovich", "Lenin Street 6", "123");
+
+                var result = commissionCar.GetInfo();
+
+                Assert.That(result, Is.EqualTo(expectedString));
+            }
+        }
+
+
 }
--- task12/CarsLibrary/CarsLibrary/Cars.cs
+++ task12/CarsLibrary/CarsLibrary/Cars.cs
@@ -45,7 +45,7 @@ namespace CarsLibrary
                 BodyType = bodyType;
             }
 
-            public string GetInfo()
+            public virtual string GetInfo()
             {
                 var body = "";
                 if (BodyType == BodyType.Sedan)
@@ -58,6 +58,80 @@ namespace CarsLibrary
                     body = "Cabriolet";
                 return $"Brand: {Brand}. Year of release: {YearOfRelease}. VIN: {VIN}. Body type: {BodyType}\n" +
                        $"Price: {Price}. Date of sale: {DateOfSale.ToShortDateString()}. Customer full name: {CustomerFullName}";
+            }
+        }
+        public class NewCar : Car
+        {
+            public string Factory { get; set; }
+
+            public TimeSpan GuaranteeTime { get; set; }
+
+            public NewCar(string brand, int yearOfRelease, string vin, BodyType bodyType, string factory,
+                TimeSpan guaranteeTime) : base(brand, yearOfRelease, vin, bodyType)
+            {
+                Factory = factory;
+                GuaranteeTime = guaranteeTime;
+            }
+
+            public override string GetInfo()
+            {
+                return base.GetInfo() + $"\n Factory: {Factory}. Guarantee time: {GuaranteeTime.TotalDays} days";
+            }
+        }
+        public enum CarCondition
+        {
+            Excellent,
+            Good,
+            Bad
+        }
+
+        public class UsedCar : Car
+        {
+            public double Mileage { get; set; }
+
+            public CarCondition Condition { get; set; }
+
+            public UsedCar(string brand, int yearOfRelease, string vin, BodyType bodyType, double mileage, CarCondition condition) : base(brand, yearOfRelease, vin,
+                bodyType)
+            {
+                Mileage = mileage;
+                Condition = condition;
+            }
+
+            private string GetConditionString()
+            {
+                var condition = "";
+                if (Condition == CarCondition.Excellent)
+                    condition = "Excellent";
+                else if (Condition == CarCondition.Good)
+                    condition = "Good";
+                else if (Condition == CarCondition.Bad)
+                    condition = "Bad";
+                return $"\n Mileage: {Mileage}. Condition: {Condition}";
+            }
+
+        }
+        public class CommissionCar : UsedCar
+        {
+            public string OwnerFullName { get; set; }
+
+            public string OwnerAddress { get; set; }
+
+            public string ContractNumber { get; set; }
+
+            public CommissionCar(string brand, int yearOfRelease, string vin, BodyType bodyType, double mileage,
+                CarCondition condition, string ownerFullName, string ownerAddress, string contractNumber) : base(brand,
+                yearOfRelease, vin, bodyType, mileage, condition)
+            {
+                OwnerFullName = ownerFullName;
+                OwnerAddress = ownerAddress;
+                ContractNumber = contractNumber;
+            }
+
+            public override string GetInfo()
+            {
+                return base.GetInfo() +
+                       $"\n Owner full name: {OwnerFullName}. Owner address: {OwnerAddress}. Contract number: {ContractNumber}.";
             }
         }
     }
